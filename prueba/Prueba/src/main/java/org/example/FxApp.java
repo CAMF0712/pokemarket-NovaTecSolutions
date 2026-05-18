6e209071e5810719bbffcaee70c94a3b061ff7a5
Marco <marco.villatoro.chacon@intel.com>
2026-05-17 18:27:36 -0600

    Ajustes de configuracion para JavaFX y SceneBuilder

---
+++ prueba/Prueba/src/main/java/org/example/FxApp.java
@@ -0,0 +1,17 @@
+package org.example;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class FxApp extends Application {
+    @Override
+    public void start(Stage stage) throws Exception {
+        FXMLLoader fxmlLoader = new FXMLLoader(FxApp.class.getResource("/org/example/prueba.fxml"));
+        Scene scene = new Scene(fxmlLoader.load());
+        stage.setTitle("Prueba JavaFX");
+        stage.setScene(scene);
+        stage.show();
+    }
+}
