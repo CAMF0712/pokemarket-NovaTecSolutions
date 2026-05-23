b2d20cedb7bb2aa47a04109442777013871226eb
Giancarlo Vega Marin <jungianca6@estudiantec.cr>
2026-05-22 20:35:58 -0600

    paginas de admin y cliente en react

--- Sprint1/sprint1/src/App.js
+++ Sprint1/sprint1/src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
+import { useState } from "react";
+import Login from "./pgs/Login";
+import AdminPg from "./pgs/AdminPg";
+import ClientePg from "./pgs/ClientePg";
+
+
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
+  const [user, setUser] = useState(null);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <Router>
+        <Routes>
+          <Route path="/login" element={<Login setUser={setUser} />} />
+
+          <Route path="/admin" element={user?.rol === "Admin" ? <AdminPg /> : <Navigate to="/login" />} />
+
+          <Route path="/cliente" element={user?.rol === "Cliente" ? <ClientePg /> : <Navigate to="/login" />} />
+
+          <Route path="*" element={<Navigate to="/login" />} />
+        </Routes>
+      </Router>
   );
 }
-
 export default App;
