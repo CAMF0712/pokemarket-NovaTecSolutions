1c514baa9d30e98cab8dc1b98813fa4003fb5738
Giancarlo Vega Marin <jungianca6@estudiantec.cr>
2026-05-31 14:45:49 -0600

    chinga a su madre java

--- prueba/Prueba/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module org.example {
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires com.google.gson;
-
-    exports org.example;
-    opens org.example to javafx.fxml , com.google.gson;
-}
