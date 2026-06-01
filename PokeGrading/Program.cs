c484ac25d075b7a4d8becef1611f4783bb9b760c
JMLoria <joloria@estudiantec.cr>
2026-05-31 21:06:02 -0600

    Creando SQL_connection.cs

--- PokeGrading/Program.cs
+++ PokeGrading/Program.cs
@@ -5,6 +5,8 @@ var builder = WebApplication.CreateBuilder(args);
 // Services
 builder.Services.AddControllers();
 
+builder.Services.AddScoped<SQL_connection>();
+
 builder.Services.AddScoped<DatabaseService>();
 
 builder.Services.AddCors(options =>
